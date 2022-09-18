735582f6e323b21b3e78e68f713f656a3d8b004d
Emmanuel Fernández <juan.fernandezdelara2865@alumnos.udg.mx>
2022-09-18 13:11:35 -0500

    Restablecí la configuración para localhost

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('https://jeflh-chat-nodejs.herokuapp.com/');
+const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
